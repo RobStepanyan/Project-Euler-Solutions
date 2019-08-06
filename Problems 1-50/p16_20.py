1367196ed937143a8f88d82c2d83634493687899
RobStepanyan <robert1stepanyan@gmail.com>
2019-08-06 17:37:53 +0400

    Finished p16_20.py

--- Problems 1-50/p16_20.py
+++ Problems 1-50/p16_20.py
@@ -110,3 +110,111 @@ def p018():
         for y in range(0, x):
             triangle[x-1][y] += max(triangle[x][y], triangle[x][y+1])
     print('p018: ', triangle[0][0])
+
+
+"""
+Counting Sundays
+
+Problem 19
+You are given the following information, but you may prefer to do some research for yourself.
+
+1 Jan 1900 was a Monday.
+Thirty days has September,
+April, June and November.
+All the rest have thirty-one,
+Saving February alone,
+Which has twenty-eight, rain or shine.
+And on leap years, twenty-nine.
+A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
+How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
+"""
+
+
+def p019():
+    def leap_year(year):
+        if year % 400 == 0 or year % 4 != 0:
+            return False
+        return True
+
+    MONTH = {
+        'January': 31, 'February': 28, 'March': 31,
+        'April': 30, 'May': 31, 'June': 30, 'July': 31,
+        'August': 31, 'September': 30, 'October': 31,
+        'November': 30, 'December': 31
+    }
+
+    WEEK_DAY = ['Monday', 'Tuesday', 'Wednesday',
+                'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+    day = 1
+    month = 'January'
+    year = 1901
+    week_day = 'Monday'
+
+    count = 0
+    while not (year == 2000 and month == 'December' and day == 31):
+        # Check if the first day of the month is Monday
+        if day == 1 and week_day == 'Monday':
+            count += 1
+
+        # Change the day, month and year if needed
+        day += 1
+        if day > MONTH[month]:
+            day = 1
+            current_month_index = list(MONTH.keys()).index(month)
+            month_keys = list(MONTH.keys())
+            if current_month_index == 11:
+                month = month_keys[0]
+            else:
+                month = month_keys[current_month_index + 1]
+
+            if month == 'January':
+                year += 1
+                if leap_year(year):
+                    MONTH['February'] = 29
+                else:
+                    MONTH['February'] = 28
+
+        # Change the weekday
+        current_weekday_index = WEEK_DAY.index(week_day)
+        if current_weekday_index == 6:
+            week_day = WEEK_DAY[0]
+        else:
+            week_day = WEEK_DAY[current_weekday_index + 1]
+
+    print('p019: ', count-1)
+
+
+"""
+Factorial digit sum
+
+Problem 20
+n! means n × (n − 1) × ... × 3 × 2 × 1
+
+For example, 10! = 10 × 9 × ... × 3 × 2 × 1 = 3628800,
+and the sum of the digits in the number 10! is 3 + 6 + 2 + 8 + 8 + 0 + 0 = 27.
+
+Find the sum of the digits in the number 100vvvv
+"""
+
+
+def p020(num=100):
+    def factorial(num):
+        prd = 1
+        for i in range(2, num+1):
+            prd *= i
+        return prd
+
+    ans = sum([int(digit) for digit in str(factorial(num))])
+    print('p020: ', ans)
+
+p016()
+# p016:  1366
+p017()
+# p017:  21124
+p018()
+# p018:  1074
+p019()
+# p019:  171
+p020()
+# p020:  648
